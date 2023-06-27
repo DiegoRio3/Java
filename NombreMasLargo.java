72f170bb7f08fd23a7c21f024fec99853278517e
DiegoRio3 <132586992+DiegoRio3@users.noreply.github.com>
2023-06-27 16:22:15 -0300

    //NombreMasLargo#6

--- NombreMasLargo.java
+++ NombreMasLargo.java
@@ -1,3 +1,12 @@
+/*
+Assignment: Obtener el nombre mas largo de tres personas, según los siguientes requerimientos
+Mediante el teclado pedir el nombre completo (nombre + apellido) de tres miembros de la familia o amigos
+usando la clase JOptionPane y método showInputDialog(). Calcular e Imprimir el nombre de la persona que tenga
+el nombre más largo (en cantidad de caracteres)   (Imprimir sólo uno de los tres, el de más caracteres en el nombre.)
+Podría usar .split(" "); del objeto String para separar nombre y apellido en un arreglo,
+y con el indice cero accedemos al nombre de la persona. Restricción no usar loops en el desarrollo de la tarea.
+Ejemplo del resultado: "Guillermo Doe tiene el nombre más largo."
+ */
 import javax.swing.*;
 
 public class NombreMasLargo {
@@ -14,13 +23,3 @@ public class NombreMasLargo {
         System.out.println(nombreMasLargo + " tiene el nombre mas largo.");
     }
 }
-
-   /* Obtener el nombre mas largo de tres personas, según los siguientes requerimientos
-        Mediante el teclado pedir el nombre completo (nombre + apellido) de tres miembros de la familia o amigos
-        usando la clase JOptionPane y método showInputDialog().
-        Calcular e Imprimir el nombre de la persona que tenga el nombre más largo (en cantidad de caracteres)
-        (Imprimir sólo uno de los tres, el de más caracteres en el nombre.)
-        Podría usar .split(" "); del objeto String para separar nombre y apellido en un arreglo,
-        y con el indice cero accedemos al nombre de la persona.
-        Restricción no usar loops en el desarrollo de la tarea.
-        Ejemplo del resultado: "Guillermo Doe tiene el nombre más largo."     */
