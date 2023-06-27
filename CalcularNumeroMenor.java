656f8693947145588e6744356836e93ffb5a27db
DiegoRio3 <132586992+DiegoRio3@users.noreply.github.com>
2023-06-27 16:23:33 -0300

    //CalcularNumeroMenor#7

--- CalcularNumeroMenor.java
+++ CalcularNumeroMenor.java
@@ -1,3 +1,13 @@
+/*
+Assignment: Calcular el numero menor de multiples valores
+El desafío es buscar el número menor de mínimo 10 valores enteros.
+Crear una clase con el método main donde el desafío es buscar el número menor de mínimo 10 valores enteros,
+usando la clase Scanner ingresar la cantidad de números a comparar,
+luego utilizando una sentencia for iterar el número de veces (ingresado) para pedir el numero entero, entonces se requiere:
+Calcular el menor número e imprimir el valor.
+Si el menor número es menor que 10, imprimir "El número menor es menor que 10!". si no,
+imprimir " el numero menor es igual o mayor que 10!".
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,21 +27,9 @@ public class CalcularNumeroMenor {
                 numMin = numeros[i];
             }
         }
-
         System.out.println("Los numero ingresados son = " + Arrays.toString(numeros));
         System.out.println("El numero mínimo es = " + numMin);
         String menor10 = numMin < 10 ? "El numero menor es menor que 10!" : "El numero menor es igual o mayor que 10!";
         System.out.println("menor10 = " + menor10);
     }
 }
-
-    /*Crear una clase con el método main donde el desafío es buscar el número menor de mínimo 10 valores enteros,
-    usando la clase Scanner ingresar la cantidad de números a comparar,
-    luego utilizando una sentencia for iterar el número de veces (ingresado) para pedir el numero entero, entonces se requiere:
-
-        Calcular el menor número e imprimir el valor.
-
-        Si el menor número es menor que 10, imprimir "El número menor es menor que 10!". si no,
-        imprimir " el numero menor es igual o mayor que 10!".
-
-     */
